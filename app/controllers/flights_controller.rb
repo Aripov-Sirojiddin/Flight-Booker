809b3d1b2e9fb54e99d992ab4fe757b07019834e
Aripov-Sirojiddin <aripov.2@osu.edu>
2024-12-17 17:05:58 -0500

    Can search and select specific flight

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,11 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all
+    if (params[:departure_airport].present? && params[:arrival_airport].present?)
+      @flights = @flights.where(
+        departure_airport_id: Airport.find_by(code: params[:departure_airport].strip),
+        arrival_airport_id: Airport.find_by(code: params[:arrival_airport].strip),
+        )
+    end
   end
 end
